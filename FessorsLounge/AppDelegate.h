9dce601ed7c254c4aa6f88c7d4d53304ce1bbee5
Stefan Brankovic <stefanbrankovic@Stefans-MacBook-Pro-4.local>
2014-02-09 14:42:06 +0100

    Initial Commit

---
+++ FessorsLounge/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FessorsLounge
+//
+//  Created by Stefan Brankovic on 09/02/14.
+//  Copyright (c) 2014 EduLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FessorsLounge/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FessorsLounge
+//
+//  Created by Stefan Brankovic on 09/02/14.
+//  Copyright (c) 2014 EduLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
